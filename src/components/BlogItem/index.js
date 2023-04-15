25ae6c4fdee61c66771296d2fe606bbedb14f1fa
prudhvi <prudhvikarimeti18@gmail.com>
2023-04-15 05:12:51 +0000

    fetchandrouting

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const BlogItem = props => {
+  const {blogItemDetails} = props
+  const {id, topic, title, imageUrl, author, avatarUrl} = blogItemDetails
+
+  return (
+    <li className="blog-item">
+      <Link to={`/blogs/${id}`} className="blog-item-link">
+        <div className="blog-item-container">
+          <img src={imageUrl} alt={`item${id}`} className="blog-item-image" />
+          <div className="blog-item-info">
+            <p className="blog-item-topic">{topic}</p>
+            <h1 className="blog-item-title">{title}</h1>
+            <div className="author-info">
+              <img className="avatar" src={avatarUrl} alt={`avatar${id}`} />
+              <p className="author-name">{author}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
+  )
+}
+export default BlogItem
